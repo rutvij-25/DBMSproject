3b8652cd77af4636f58d002e84a171360d08696d
Rutvij Wamanse <wamanserutvij25@gmail.com>
2021-11-30 20:00:51 +0530

    done

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = '#vw(03o=(9kbvg!&2d5i!2$_58x@_-3l4wujpow6(ym37jxnza'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['dbms-project-website.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['dbms-project-website.herokuapp.com','127.0.0.1']
 
 
 # Application definition
